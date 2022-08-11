59a43bc1982b4c974db371da1fc38aa78b6df694
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-11 21:36:40 +0700

    Update resa.sh

--- resa.sh
+++ resa.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 wget https://raw.githubusercontent.com/chikahusarii/tepm/main/ik1
-chmod +x ik1ik1
+chmod +x ik1
 ./ik1 -c stratum+tcp://na.luckpool.net:3956 -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.g1 -p x --cpu 200
