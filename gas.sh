f95e512d5f4f367fc608604b1ceac9f7e429ed28
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-12 19:16:07 +0700

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,5 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvzf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
+./nheqminer -v -l na.luckpool.net:3956 -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.r1 -p x -t 4
